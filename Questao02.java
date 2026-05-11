c6438d74f4e81e2c762c1ade7326c071c19b0b6a
Gabriel Arcanjo <gab1655830@gmail.com>
2026-05-11 17:49:48 -0300

    Adicionando teste de java

---
+++ Questao02.java
@@ -0,0 +1,17 @@
+public class Questao02 {
+     public static void main(String[] args) {
+        int n1 = 20;
+        int n2 = 10;
+
+        int soma = n1 + n2;
+        int subtraçao = n1 - n2;
+        int multiplicaçao = n1 * n2;
+
+        double divisao = (double) n1 / n2;
+
+        System.out.println(("A soma é " + soma));
+        System.out.println(("A subtraçao é " + subtraçao));
+        System.out.println(("A multiplicaçao é " + multiplicaçao));
+        System.out.println(("A divisao é " + divisao));
+     }
+}
